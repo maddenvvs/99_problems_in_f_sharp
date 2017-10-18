1b77f103cf7b5d8b8dbaabae468b5f84014083b6
maddenvvs <maddenvvs@gmail.com>
2017-10-18 12:25:20 +0400

    Solved third problem

--- NinetyNineProblems.Solved/Lists.fs
+++ NinetyNineProblems.Solved/Lists.fs
@@ -11,3 +11,10 @@ module Lists =
         | [] | [_] -> None
         | [a; b] -> Some (a, b)
         | _ :: xs -> last_two xs
+
+    let rec element_at k lst =
+        match k, lst with
+        | k, _ when k < 1 -> None
+        | _, [] -> None
+        | 1, x :: _ -> Some x
+        | k, _ :: xs -> element_at (k-1) xs
--- NinetyNineProblems.Tests/Lists_Tests.fs
+++ NinetyNineProblems.Tests/Lists_Tests.fs
@@ -31,3 +31,19 @@ let ``Lists.last_two on list with two elements returns these elements`` () =
 [<Fact>]
 let ``Lists.last_two on list with three elements returns last two elements`` () =
     Assert.Equal(Some (2, 3), Lists.last_two [1; 2; 3])
+
+[<Fact>]
+let ``Lists.element_at returns None if position is lower than 1`` () =
+    Assert.Equal(None, Lists.element_at 0 [1; 2; 3])
+
+[<Fact>]
+let ``Lists.element_at returns None if list is empty`` () =
+    Assert.Equal(None, Lists.element_at 1 [])
+
+[<Fact>]
+let ``Lists.element_at returns None if index is greater than list length`` () =
+    Assert.Equal(None, Lists.element_at 3 [1; 2])
+
+[<Fact>]
+let ``Lists.element_at returns element if index is lower than list length`` () =
+    Assert.Equal(Some 2, Lists.element_at 2 [1; 2; 3])
