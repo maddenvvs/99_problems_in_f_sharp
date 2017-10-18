e365db3a1e766c3a7077f2f9c91f4ec3930e416f
maddenvvs <maddenvvs@gmail.com>
2017-10-18 12:14:31 +0400

    Refactored tests

--- NinetyNineProblems.Tests/Lists_Tests.fs
+++ NinetyNineProblems.Tests/Lists_Tests.fs
@@ -2,32 +2,32 @@ module Lists_Tests
 
 open System
 open Xunit
-open NinetyNineProblems.Solved.Lists
+open NinetyNineProblems.Solved
 
 [<Fact>]
-let ``last on empty list returns None`` () =
-    Assert.Equal(None, last [])
+let ``Lists.last on empty list returns None`` () =
+    Assert.Equal(None, Lists.last [])
 
 [<Fact>]
-let ``last on list with one element returns this element`` () =
-    Assert.Equal(Some 1, last [1])
+let ``Lists.last on list with one element returns this element`` () =
+    Assert.Equal(Some 1, Lists.last [1])
 
 [<Fact>]
-let ``last on list with two elements returns the last one element`` () =
-    Assert.Equal(Some 2, last [1; 2])
+let ``Lists.last on list with two elements returns the last one element`` () =
+    Assert.Equal(Some 2, Lists.last [1; 2])
 
 [<Fact>]
-let ``last_two on empty list returns None`` () =
-    Assert.Equal(None, last_two [])
+let ``Lists.last_two on empty list returns None`` () =
+    Assert.Equal(None, Lists.last_two [])
 
 [<Fact>]
-let ``last_two on list with one element returns None`` () =
-    Assert.Equal(None, last_two [1])
+let ``Lists.last_two on list with one element returns None`` () =
+    Assert.Equal(None, Lists.last_two [1])
 
 [<Fact>]
-let ``last_two on list with two elements returns these elements`` () =
-    Assert.Equal(Some (1, 2), last_two [1; 2])
+let ``Lists.last_two on list with two elements returns these elements`` () =
+    Assert.Equal(Some (1, 2), Lists.last_two [1; 2])
 
 [<Fact>]
-let ``last_two on list with three elements returns last two elements`` () =
-    Assert.Equal(Some (2, 3), last_two [1; 2; 3])
+let ``Lists.last_two on list with three elements returns last two elements`` () =
+    Assert.Equal(Some (2, 3), Lists.last_two [1; 2; 3])
