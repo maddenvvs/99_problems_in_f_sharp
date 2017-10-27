234a1ddefadeb0b5d4e3f180026613875793972c
maddenvvs <maddenvvs@gmail.com>
2017-10-27 13:16:38 +0400

    Solved problem 2.02

--- NinetyNineProblems.Solved/Arithmetic.fs
+++ NinetyNineProblems.Solved/Arithmetic.fs
@@ -12,4 +12,18 @@ module Arithmetic =
         | n when n % 2 = 0 -> false
         | n ->
             let upper = (double n) |> sqrt |> ceil |> int
-            {2..upper} |> Seq.forall (fun dv -> n % dv <> 0)
+            {3..2..upper} |> Seq.forall (fun dv -> n % dv <> 0)
+
+    (*
+        2.02
+        Determine the prime factors of a given positive integer.
+    *)
+    let primeFactors n =
+        let rec aux d acc = function
+            | k when k < 2 -> acc
+            | k when k % 2 = 0 -> aux d (2 :: acc) (k / 2)
+            | k ->
+                if k % d = 0
+                then aux d (d :: acc) (k / d)
+                else aux (d + 2) acc k
+        List.rev (aux 3 [] n)
--- NinetyNineProblems.Tests/Arithmetic_Tests.fs
+++ NinetyNineProblems.Tests/Arithmetic_Tests.fs
@@ -39,3 +39,29 @@ let ``isPrime identifies non-primes correctly`` n =
 [<InlineData(-200_000)>]
 let ``isPrime identifies negative numbers as non-primes`` n =
     Assert.Equal(false, isPrime n)
+
+[<Theory>]
+[<InlineData(1)>]
+[<InlineData(0)>]
+[<InlineData(-1)>]
+[<InlineData(-17)>]
+[<InlineData(-1000)>]
+let ``primeFactors returns empty list for numbers less then 2`` n =
+    Assert.True([] = primeFactors n)
+
+[<Theory>]
+[<InlineData(3)>]
+[<InlineData(5)>]
+[<InlineData(7)>]
+[<InlineData(11)>]
+[<InlineData(7919)>]
+[<InlineData(104_729)>]
+[<InlineData(2_147_483_647)>]
+let ``primeFactors returns list with one number for prime numbes`` n =
+    Assert.True([n] = primeFactors n)
+
+let ``primeFactors returns list with dividers in ascending order`` () =
+    Assert.True([3; 3; 5; 7] = primeFactors 315)
+
+let ``primeFactors works well with large numbers`` () =
+    Assert.True([3; 3; 239; 4649] = primeFactors 9999999)
