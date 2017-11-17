54548650f514eaf296781f4285760cfce522bdb7
maddenvvs <maddenvvs@gmail.com>
2017-11-17 22:33:56 +0400

    Scaffolded multiway tree source code

---
+++ NinetyNineProblems.Solved/MultiwayTrees.fs
@@ -0,0 +1,9 @@
+namespace NinetyNineProblems.Solved
+
+module MultiwayTrees =
+
+    (*
+        5.01
+        Check whether a given term represents a multiway tree.
+    *)
+    let isMultiwayTree tree = failwith "Not implemented"
