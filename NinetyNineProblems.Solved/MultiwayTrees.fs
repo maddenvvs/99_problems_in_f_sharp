5700ec13d6e7ec5a4aed595c5fca2c8c75772936
maddenvvs <maddenvvs@gmail.com>
2017-11-19 19:00:14 +0400

    Solved problems 5.01, 5.02

--- NinetyNineProblems.Solved/MultiwayTrees.fs
+++ NinetyNineProblems.Solved/MultiwayTrees.fs
@@ -2,8 +2,18 @@ namespace NinetyNineProblems.Solved
 
 module MultiwayTrees =
 
+    type mtree<'a> =
+        MTree of 'a * (mtree<'a> list)
+
     (*
         5.01
         Check whether a given term represents a multiway tree.
     *)
-    let isMultiwayTree tree = failwith "Not implemented"
+    let isMultiwayTree (MTree _) = true
+
+    (*
+        5.02
+        Count the nodes of a multiway tree.
+    *)
+    let rec countNodes (MTree (_, children)) =
+        List.fold (fun t child -> t + (countNodes child)) 1 children
