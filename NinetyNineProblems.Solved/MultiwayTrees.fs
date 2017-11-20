ceea24909714d1a586515b0f76f400b08fdab181
maddenvvs <maddenvvs@gmail.com>
2017-11-20 17:43:27 +0400

    Solved problem 5.04

--- NinetyNineProblems.Solved/MultiwayTrees.fs
+++ NinetyNineProblems.Solved/MultiwayTrees.fs
@@ -17,3 +17,18 @@ module MultiwayTrees =
     *)
     let rec countNodes (MTree (_, children)) =
         List.fold (fun t child -> t + (countNodes child)) 1 children
+
+    (*
+        5.03
+        Tree construction from a node string.
+    *)
+    let constructMTree str = failwith "Not implemented"
+
+    (*
+        5.04
+        Determine the internal path length of a tree.
+    *)
+    let rec iplAux len (T(_, sub)) =
+        List.fold_left (fun sum t -> sum + iplAux (len + 1) t) len sub
+
+    let ipl t = iplAux 0 t
